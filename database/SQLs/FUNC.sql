3e25dc079a398b557f92ce4a72cceb6c13383b62
Roman Dyakov <dyakovri@gmail.com>
2020-03-29 16:42:41 +0300

    FUNCTION Autoprove added

--- database/SQLs/FUNC.sql
+++ database/SQLs/FUNC.sql
@@ -1,4 +1,8 @@
--- Returns user id if login succeed, else returns NULL
+--------------------------------------------------------
+-- Работа с пользователями
+--------------------------------------------------------
+-- Returns user id if login succeed, 
+-- else rases error
 DROP FUNCTION IF EXISTS users.login;
 CREATE OR REPLACE FUNCTION users.login(
     IN p_type_id INTEGER,
@@ -29,7 +33,8 @@ END;
 $$ LANGUAGE plpgsql;
 
 
--- Returns user id if no such user already exists, else returns error
+-- Returns new user id if no such user already exists, 
+-- else raises error
 DROP FUNCTION IF EXISTS users.register;
 CREATE OR REPLACE FUNCTION users.register(
     IN p_type_id INTEGER,
@@ -59,3 +64,52 @@ BEGIN
     RETURN newid;
 END;
 $$ LANGUAGE plpgsql;
+
+
+-- Returns True if autoprove completed successfully and adds approve record
+-- Else returns False
+DROP FUNCTION IF EXISTS users.prove_auto;
+CREATE OR REPLACE FUNCTION users.prove_auto(
+    IN p_user_id INTEGER
+) RETURNS BOOLEAN AS $$
+DECLARE 
+    p_lastname VARCHAR(64);
+    res BOOLEAN;
+    ll users.login%rowtype;
+BEGIN
+    res := FALSE;
+    --
+    IF p_user_id NOT IN (SELECT DISTINCT up.id FROM users.profile up)
+    THEN RAISE EXCEPTION 'User is''t exists';
+    END IF;
+    --
+    p_lastname := (SELECT vpa.lastname 
+    FROM users.v_profile_available vpa 
+    WHERE id = p_user_id);
+    --
+    FOR ll IN 
+        (SELECT *
+        FROM users.login ul 
+        WHERE ul.user_id = p_user_id)
+    LOOP 
+        IF (SELECT COUNT(DISTINCT pp.id) = 1 FROM users.preregistred_profile pp
+            WHERE (pp.type_id, pp.password, TRIM(UPPER(pp.lastname))) = (ll.type_id, ll.password, TRIM(UPPER(p_lastname))))
+        THEN 
+            res := TRUE;
+            INSERT INTO users.approval (login_id, is_proved, expires) VALUES (
+                ll.id,
+                TRUE,
+                (SELECT MAX(pp2.expires)
+                    FROM users.preregistred_profile pp2
+                    WHERE (pp2.type_id, pp2.password, pp2.lastname) = (ll.type_id, ll.password, p_lastname))
+            );
+        END IF;
+    END LOOP;
+    --
+    RETURN res;
+END;
+$$ LANGUAGE plpgsql;
+
+--------------------------------------------------------
+-- Работа с файлом
+--------------------------------------------------------
